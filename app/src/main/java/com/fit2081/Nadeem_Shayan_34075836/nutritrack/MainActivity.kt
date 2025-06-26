3715b785fac483be021dbb5a5db9324e66ed77b6
Fresh <shayannadeem1x@icloud.com>
2025-06-26 12:13:47 +0400

    Updating name

--- app/src/main/java/com/fit2081/Nadeem_Shayan_34075836/nutritrack/MainActivity.kt
+++ app/src/main/java/com/fit2081/Nadeem_Shayan_34075836/nutritrack/MainActivity.kt
@@ -135,7 +135,7 @@ fun WelcomePage(modifier: Modifier = Modifier, loginViewModel: RegisterAndLoginS
             )
             Spacer(modifier = Modifier.height(20.dp))
             Text(
-                text = "Vita",
+                text = "NutriTrack",
                 fontSize = 43.sp,
                 fontWeight = FontWeight.Bold,
                 fontFamily = sfProFont
